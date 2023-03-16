ac112567055d2ff58f82d4bb5c7b067d567c3c81
André M <aemtechdeck@gmail.com>
2023-03-15 21:05:35 -0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,22 @@
+const qdpi = document.getElementById("qdpi");
+let inputA = document.getElementById("dpi");
+let inputB = document.getElementById("sens");
+
+// fazer os calculos ----------------------->
+
+const calculoqdpi = (a, b) => (a * b) / 400;
+const calculoodpi = (a, b) => (a * b) / 800;
+const calculomdpi = (a, b) => (a * b) / 1600;
+const calculoEdpi = (a, b) => a * b;
+
+const calcular = () => {
+  let a = parseFloat(inputA.value);
+  let b = parseFloat(inputB.value);
+
+  qdpi.innerHTML = calculoqdpi(a, b).toFixed(3);
+  odpi.innerHTML = calculoodpi(a, b).toFixed(3);
+  mdpi.innerHTML = calculomdpi(b, a).toFixed(3);
+  edpi.innerHTML = calculoEdpi(b, a).toFixed(3);
+};
+
+inputB.addEventListener("input", calcular);
